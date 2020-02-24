3845299faf95161cd25c023db4f01f6000b3bd94
jmessare46 <jmessare46@gmail.com>
2020-02-24 16:40:02 -0500

    Updated sql file with naming scheme

--- alumni.sql
+++ alumni.sql
@@ -1,166 +1,158 @@
--- phpMyAdmin SQL Dump
--- version 4.8.3
--- https://www.phpmyadmin.net/
+CREATE DATABASE  IF NOT EXISTS `sdd_primary` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `sdd_primary`;
+-- MySQL dump 10.13  Distrib 8.0.16, for macos10.14 (x86_64)
 --
--- Host: 127.0.0.1
--- Generation Time: Feb 24, 2020 at 12:21 AM
--- Server version: 10.1.35-MariaDB
--- PHP Version: 7.2.9
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: sdd_primary
+-- ------------------------------------------------------
+-- Server version	8.0.16
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `sdd_primary`
---
-CREATE DATABASE IF NOT EXISTS `sdd_primary` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
-USE `sdd_primary`;
-
--- --------------------------------------------------------
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `academic_field`
 --
 
 DROP TABLE IF EXISTS `academic_field`;
-CREATE TABLE IF NOT EXISTS `academic_field` (
-  `field_id` int(11) NOT NULL AUTO_INCREMENT,
-  `field_name` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `description` mediumtext COLLATE utf8_bin,
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `academic_field` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `field_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `description` mediumtext CHARACTER SET utf8 COLLATE utf8_bin,
   `major` tinyint(4) DEFAULT NULL,
   `minor` tinyint(4) DEFAULT NULL,
   `concentration` tinyint(4) DEFAULT NULL,
-  PRIMARY KEY (`field_id`),
-  UNIQUE KEY `field_id` (`field_id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `field_id` (`id`),
+  UNIQUE KEY `academic_field_field_name_uindex` (`field_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
--- --------------------------------------------------------
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `account_type`
 --
 
 DROP TABLE IF EXISTS `account_type`;
-CREATE TABLE IF NOT EXISTS `account_type` (
-  `acct_id` int(11) NOT NULL AUTO_INCREMENT,
-  `type_name` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  PRIMARY KEY (`acct_id`),
-  UNIQUE KEY `acct_id` (`acct_id`)
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `account_type` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `acct_id` (`id`),
+  UNIQUE KEY `account_type_type_name_uindex` (`type_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Table structure for table `club_roster`
+--
+
+DROP TABLE IF EXISTS `club_roster`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `club_roster` (
+  `club_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `added_on` datetime DEFAULT NULL,
+  PRIMARY KEY (`club_id`,`user_id`),
+  KEY `roster2users` (`user_id`),
+  CONSTRAINT `club_roster_clubs_id_fk` FOREIGN KEY (`club_id`) REFERENCES `clubs` (`id`),
+  CONSTRAINT `club_roster_users_id_fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `clubs`
 --
 
 DROP TABLE IF EXISTS `clubs`;
-CREATE TABLE IF NOT EXISTS `clubs` (
-  `club_id` int(11) NOT NULL AUTO_INCREMENT,
-  `club_name` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `description` mediumtext COLLATE utf8_bin,
-  PRIMARY KEY (`club_id`),
-  UNIQUE KEY `club_id` (`club_id`)
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `clubs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `club_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `description` mediumtext CHARACTER SET utf8 COLLATE utf8_bin,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `club_id` (`id`),
+  UNIQUE KEY `clubs_club_name_uindex` (`club_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `club_roaster`
---
-
-DROP TABLE IF EXISTS `club_roaster`;
-CREATE TABLE IF NOT EXISTS `club_roaster` (
-  `club_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `added_on` datetime DEFAULT NULL,
-  PRIMARY KEY (`club_id`,`user_id`),
-  KEY `roster2users` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
--- --------------------------------------------------------
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `sessions`
 --
 
 DROP TABLE IF EXISTS `sessions`;
-CREATE TABLE IF NOT EXISTS `sessions` (
-  `session_id` varchar(255) COLLATE utf8_bin NOT NULL,
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `sessions` (
+  `id` varchar(255) COLLATE utf8_bin NOT NULL,
   `user_id` int(11) NOT NULL,
   `expiration` datetime DEFAULT NULL,
   PRIMARY KEY (`user_id`),
-  UNIQUE KEY `session_id` (`session_id`)
+  UNIQUE KEY `session_id` (`id`),
+  CONSTRAINT `sessions_users_id_fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
--- --------------------------------------------------------
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `users`
 --
 
 DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `user_id` int(11) NOT NULL AUTO_INCREMENT,
-  `email` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `password` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   `grad_year` int(11) DEFAULT NULL,
-  `industry` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `industry` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   `gpa` float DEFAULT NULL,
   `salary` float DEFAULT NULL,
-  `picture` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `bio` mediumtext COLLATE utf8_bin,
-  `comm_context` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `picture` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `bio` mediumtext CHARACTER SET utf8 COLLATE utf8_bin,
+  `comm_context` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   `active` tinyint(4) DEFAULT NULL,
   `last_login` datetime DEFAULT NULL,
-  `last_login_ip` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `last_login_ip` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   `major_primary` int(11) DEFAULT NULL,
   `major_secondary` int(11) DEFAULT NULL,
   `minor` int(11) DEFAULT NULL,
   `concentration` int(11) DEFAULT NULL,
-  `acct_type` int(11) DEFAULT NULL,
-  `region` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  PRIMARY KEY (`user_id`),
-  UNIQUE KEY `user_id_2` (`user_id`),
-  KEY `user_id` (`user_id`),
+  `account_type_id` int(11) DEFAULT NULL,
+  `region` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_id_2` (`id`),
+  KEY `user_id` (`id`),
   KEY `primary_major` (`major_primary`),
   KEY `secondary_major` (`major_secondary`),
   KEY `concentration` (`concentration`),
   KEY `minor` (`minor`),
-  KEY `users2accounts` (`acct_type`),
-  KEY `user_id_3` (`user_id`)
+  KEY `users2accounts` (`account_type_id`),
+  KEY `user_id_3` (`id`),
+  CONSTRAINT `users_account_type_id_fk` FOREIGN KEY (`account_type_id`) REFERENCES `account_type` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `club_roaster`
---
-ALTER TABLE `club_roaster`
-  ADD CONSTRAINT `roster2club` FOREIGN KEY (`club_id`) REFERENCES `clubs` (`club_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `roster2users` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Constraints for table `users`
---
-ALTER TABLE `users`
-  ADD CONSTRAINT `concentration` FOREIGN KEY (`concentration`) REFERENCES `academic_field` (`field_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `minor` FOREIGN KEY (`minor`) REFERENCES `academic_field` (`field_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `primary_major` FOREIGN KEY (`major_primary`) REFERENCES `academic_field` (`field_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `secondary_major` FOREIGN KEY (`major_secondary`) REFERENCES `academic_field` (`field_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `users2accounts` FOREIGN KEY (`acct_type`) REFERENCES `account_type` (`acct_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-02-24 16:38:17
+
