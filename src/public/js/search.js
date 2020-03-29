0030226320fe7ecb0a2931b01b4f956ad442fe1d
Joseph Messare <jmessare46@gmail.com>
2020-03-29 12:45:26 -0400

    Fixed line endings

--- src/public/js/search.js
+++ src/public/js/search.js
@@ -1,5 +1,5 @@
 // Define some test data
-var cheeseData = [
+const cheeseData = [
     {id:1, type:"Brie", rind:"mould", age:"4 weeks", color:"white", image:"brie.jpg"},
     {id:2, type:"Cheddar", rind:"none", age:"1 year", color:"yellow", image:"cheddar.jpg"},
     {id:3, type:"Gouda", rind:"wax", age:"6 months", color:"cream", image:"gouda.jpg"},
@@ -7,7 +7,7 @@ var cheeseData = [
 ];
 
 //define Tabulator
-var table = new Tabulator("#search_table", {
+const table = new Tabulator("#search_table", {
     height:"311px",
     layout:"fitColumns",
     resizableColumns:false,
@@ -15,9 +15,9 @@ var table = new Tabulator("#search_table", {
         {title:"Cheese", field:"type", sorter:"string"},
     ],
     rowFormatter:function(row){
-        let element = row.getElement();
-        let width = element.offsetWidth;
-        let data = row.getData();
+        const element = row.getElement();
+        const width = element.offsetWidth;
+        const data = row.getData();
         let rowTable;
 
         //clear current row data
@@ -26,7 +26,7 @@ var table = new Tabulator("#search_table", {
         //define a table layout structure and set width of row
         rowTable = document.createElement("table");
         rowTable.style.width = (width - 18) + "px";
-        let rowTabletr = document.createElement("tr");
+        const rowTabletr = document.createElement("tr");
 
         //add image on left of row
         let cellContents = "<td><img src='/images/logo.png'></td>";
